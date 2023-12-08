8bc349e53678961ba54bd67993082466d919449c
Gloria Macia <gloria.macia@roche.com>
2023-12-08 13:01:11 +0100

    update model engine

--- genai/create.py
+++ genai/create.py
@@ -19,12 +19,12 @@ if 'token' not in st.session_state:
             <a href="{st.secrets.APP_URI}" target = "_self"> 
             sign in </a>""", unsafe_allow_html=True)
 else:
-    st.write(st.session_state['token'])
+    # st.write(st.session_state['token'])
     # Set up our connection to the API.
     stability_api = client.StabilityInference(
         key=st.secrets["stability_key"], # API Key reference.
         verbose=True, # Print debug messages.
-        engine="stable-diffusion-xl-1024-v1-0", # Set the engine to use for generation.
+        engine="stable-diffusion-xl-1024-v0-9", # Set the engine to use for generation.
         # Check out the following link for a list of available engines: https://platform.stability.ai/docs/features/api-parameters#engine
     )
 
@@ -43,19 +43,19 @@ else:
         with st_lottie_spinner(lottie, key="download"):
         # Open the image using Pillow
             img1 = Image.open(image)
-            img1 = img1.resize((512, 512))
+            img1 = img1.resize((256, 256))
             answers = stability_api.generate(
             prompt=prompt,
             init_image=img1, # Assign our previously generated img as our Initial Image for transformation.
             start_schedule=0.6, # Set the strength of our prompt in relation to our initial image.
-            seed=12345, # If attempting to transform an image that was previously generated with our API,
+            seed=42, # If attempting to transform an image that was previously generated with our API,
                             # initial images benefit from having their own distinct seed rather than using the seed of the original image generation.
             steps=50, # Amount of inference steps performed on image generation. Defaults to 30.
             cfg_scale=7, # Influences how strongly your generation is guided to match your prompt.
                         # Setting this value higher increases the strength in which it tries to match your prompt.
                         # Defaults to 7.0 if not specified.
-            width=512, # Generation width, defaults to 512 if not included.
-            height=512, # Generation height, defaults to 512 if not included.
+            width=256, # Generation width, defaults to 512 if not included.
+            height=256, # Generation height, defaults to 512 if not included.
             sampler=generation.SAMPLER_K_DPMPP_2M # Choose which sampler we want to denoise our generation with.
                                                         # Defaults to k_dpmpp_2m if not specified. Clip Guidance only supports ancestral samplers.
                                                         # (Available Samplers: ddim, plms, k_euler, k_euler_ancestral, k_heun, k_dpm_2, k_dpm_2_ancestral, k_dpmpp_2s_ancestral, k_lms, k_dpmpp_2m, k_dpmpp_sde)
@@ -77,7 +77,7 @@ else:
                             img2=img2,
                             label1="Original",
                             label2="Gen AI",
-                            width=512,
+                            width=256,
                             starting_position=50,
                             show_labels=True,
                             make_responsive=True,
